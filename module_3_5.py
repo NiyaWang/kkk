cdaba4fb90f511ae40aab3289231db421d3dc07f
NiyaWang <155570863+NiyaWang@users.noreply.github.com>
2024-09-23 13:07:26 +0800

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -2,6 +2,8 @@ def get_multiplied_digits(number):
     str_number = str(number)
 
     first = int(str_number[0])
+    while str_number.endswith('0'):
+        str_number = str_number[:len(str_number) - 1]
 
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
@@ -10,4 +12,5 @@ def get_multiplied_digits(number):
 
 
 result = get_multiplied_digits(40203)
+result = get_multiplied_digits(420)
 print(result)
